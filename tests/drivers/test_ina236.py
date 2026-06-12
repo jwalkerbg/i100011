d66f1590cf4c09ac7d09586e273afe6b3f7840cd
imc <i_cenov@botevgrad.com>
2026-06-12 12:58:14 +0300

    RT #153: Removed obsolete test.

--- tests/drivers/test_ina236.py
+++
@@ -1,9 +0,0 @@
-# test_utilities.py
-
-import unittest
-
-from tbench_sma import drivers
-
-class TestDrivers(unittest.TestCase):
-    def test_hello_from_ina236(self):
-        self.assertEqual(drivers.hello_from_ina236(),None)
